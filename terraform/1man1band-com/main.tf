d95a78e02fe7c817d23d988a3c613cc17f584dec
pypeaday <github@pype.aleeas.com>
2024-04-11 07:07:33 -0500

    move 1man1band.com to cf pages

--- terraform/1man1band-com/main.tf
+++ terraform/1man1band-com/main.tf
@@ -1,48 +1,81 @@
-# variable "github_token" {}
-# variable "github_user" {}
-#
-# provider "github" {
-#   owner = var.github_user
-#   token = var.github_token
-# }
-#
-# resource "github_repository" "digital-harbor-josh" {
-#   name        = "digital-harbor-josh"
-#   description = "Josh's digital harbor"
-#
-#   visibility = "public"
-#
-#   # pages {
-#   #   build_type = "workflow"
-#   #   cname      = "1man1band.com"
-#   # }
-# }
-
 provider "namecheap" {
   user_name = var.username
   api_user  = var.username
   api_key   = var.api_key
 }
+provider "namecheapecosystem" {
+  username  = var.username
+  api_user  = var.username
+  api_token = var.api_key
+}
 
-resource "namecheap_domain_records" "_1man1band-com" {
-  domain     = "1man1band.com"
-  email_type = "NONE"
+# Create a new Namecheap domain DNS
+resource "namecheap_domain_dns" "mydns" {
+  provider = namecheapecosystem
+  domain   = var.domain
 
-  # # GH Pages servers
-  # dynamic "record" {
-  #   for_each = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
-  #
-  #   content {
-  #     hostname = "@"
-  #     type     = "A"
-  #     address  = record.value
-  #   }
-  # }
+  nameservers = cloudflare_zone.my_zone.name_servers
+}
 
-  record {
-    hostname = "www"
-    type     = "CNAME"
-    address  = "1man1.band"
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+data "cloudflare_accounts" "cloudflare_account_data" {
+  name = "cloudflare@pype.aleeas.com"
+}
+
+# Cloudflare Pages project with managing build config
+resource "cloudflare_pages_project" "build_config" {
+  account_id        = var.cloudflare_account_id
+  name              = replace(var.domain, ".", "-")
+  production_branch = "main"
+  source {
+    type = "github"
+    config {
+      owner = "pypeaday"
+      # owner                         = "DigitalHarbor7"
+      repo_name                     = "DigitalHarbor"
+      production_branch             = "main"
+      production_deployment_enabled = true
+    }
   }
+  build_config {
+    root_dir = "_site/1man1band"
+  }
+  deployment_configs {
+    # preview {}
+    production {}
+  }
+}
+resource "cloudflare_pages_domain" "my_domain" {
+  account_id   = var.cloudflare_account_id
+  project_name = cloudflare_pages_project.build_config.name
+  domain       = var.domain
+}
+
+resource "cloudflare_zone" "my_zone" {
+  account_id = var.cloudflare_account_id
+  zone       = var.domain
+}
+
+resource "cloudflare_record" "my_record_www" {
+  zone_id         = cloudflare_zone.my_zone.id
+  name            = "www"
+  value           = var.domain
+  type            = "CNAME"
+  proxied         = true
+  ttl             = 1
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "my_record" {
+  zone_id         = cloudflare_zone.my_zone.id
+  name            = var.domain
+  value           = "${replace(var.domain, ".", "-")}.pages.dev"
+  type            = "CNAME"
+  proxied         = true
+  ttl             = 1
+  allow_overwrite = true
 }
 
--- terraform/1man1band-com/variables.tf
+++ terraform/1man1band-com/variables.tf
@@ -1,4 +1,8 @@
+variable "domain" {
+  default = "1man1band.com"
+}
 variable "username" {}
 variable "api_key" {}
+variable "cloudflare_account_id" {}
 variable "cloudflare_api_token" {}
 variable "cloudflare_email" {}
