c477676402d5c74b20c898f2fee7dd09c43123bc
pypeaday <github@pype.aleeas.com>
2024-04-12 06:31:24 -0500

    moved

--- terraform/digital-harbor-com/import.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-terraform import -var-file ../.tf.vars namecheap_domain_records.digital-harbor-com mydigitalharbor.com
--- terraform/digital-harbor-com/providers.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    namecheap = {
-      source  = "namecheap/namecheap"
-      version = ">= 2.0.0"
-    }
-  }
-}
