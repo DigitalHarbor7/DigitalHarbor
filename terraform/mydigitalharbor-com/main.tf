4e3114c11f0cc497fcb15e41fe3d9addee8be7c3
pypeaday <github@pype.aleeas.com>
2024-04-12 16:32:56 -0500

    refactor some terraforma nd give my pages a develop subdomain for develop branch

--- terraform/mydigitalharbor-com/main.tf
+++ terraform/mydigitalharbor-com/main.tf
@@ -4,34 +4,12 @@ provider "namecheap" {
   api_key   = var.api_key
 }
 
-# resource "namecheap_domain_records" "digital-harbor-com" {
-#   domain     = "mydigitalharbor.com"
-#   email_type = "NONE"
-
-#   dynamic "record" {
-#     # GH Pages servers
-#     for_each = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
-
-#     content {
-#       hostname = "@"
-#       type     = "A"
-#       address  = record.value
-#     }
-#   }
-
-#   record {
-#     hostname = "www"
-#     type     = "CNAME"
-#     address  = "digitalharbor7.github.io"
-#   }
-# }
-
-
 provider "namecheapecosystem" {
   username  = var.username
   api_user  = var.username
   api_token = var.api_key
 }
+
 # Create a new Namecheap domain DNS
 resource "namecheap_domain_dns" "mydns" {
   provider = namecheapecosystem
@@ -60,13 +38,14 @@ resource "cloudflare_pages_project" "build_config" {
       repo_name                     = "DigitalHarbor"
       production_branch             = "main"
       production_deployment_enabled = true
+      preview_branch_includes       = ["develop"]
     }
   }
   build_config {
     root_dir = "_site"
   }
   deployment_configs {
-    # preview {}
+    preview {}
     production {}
   }
 }
@@ -80,12 +59,26 @@ resource "cloudflare_pages_domain" "www_my_domain" {
   project_name = cloudflare_pages_project.build_config.name
   domain       = "www.${var.domain}"
 }
+resource "cloudflare_pages_domain" "develop_my_domain" {
+  account_id   = var.cloudflare_account_id
+  project_name = cloudflare_pages_project.build_config.name
+  domain       = "develop.${var.domain}"
+}
 
 resource "cloudflare_zone" "my_zone" {
   account_id = var.cloudflare_account_id
   zone       = var.domain
 }
 
+resource "cloudflare_record" "my_record_develop" {
+  zone_id         = cloudflare_zone.my_zone.id
+  name            = "develop"
+  value           = "develop.${replace(var.domain, ".", "-")}.pages.dev"
+  type            = "CNAME"
+  proxied         = true
+  ttl             = 1
+  allow_overwrite = true
+}
 resource "cloudflare_record" "my_record_www" {
   zone_id         = cloudflare_zone.my_zone.id
   name            = "www"
